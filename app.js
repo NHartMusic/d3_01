56f037c2ad21a90164352e8d832349ada36593fc
Nick Hart <nhartmusic@gmail.com>
2020-06-04 18:23:30 +0200

    nice lil graph

--- app.js
+++ app.js
@@ -5,9 +5,28 @@ const DUMMY_DATA = [
     { id: 'd4', value: 6, region: 'Palestine' },
 ]
 
-d3.select('div')
-    .selectAll('p')
+const xScale = d3
+    .scaleBand()
+    .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+    .rangeRound([0, 250])
+    .padding(0.1)
+
+const yScale = d3
+    .scaleLinear()
+    .domain([0, 15])
+    .range([200, 0])
+
+const container = d3
+    .select('svg')
+    .classed('container', true)
+
+const bars = container
+    .selectAll('div')
     .data(DUMMY_DATA)
     .enter()
-    .append('p')
-    .text(dta => dta.region)
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', xScale.bandwidth())
+    .attr('height', (data) => 200 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value))
